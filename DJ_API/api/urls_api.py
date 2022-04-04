3bcf6bae95581711b8cbf977a15f671308767c12
Crhispo <cristianandrade2001@gmail.com>
2022-04-04 14:27:51 -0500

    Se actualizo la url para sopartar id´s

--- DJ_API/api/urls_api.py
+++ DJ_API/api/urls_api.py
@@ -3,4 +3,5 @@ from DJ_API.api.views import CompaniaView
 
 urlpatterns = [
     path('compañias/', CompaniaView.as_view(), name='ListaCompanias'),
+    path('compañias/<int:id>', CompaniaView.as_view(), name='ListarCompania'),
 ]
